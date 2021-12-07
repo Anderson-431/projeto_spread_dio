8dc8ce4de9cb0417afe9ad0f15130be2e33c277a
Anderson Pinto <anderson.p20@gmail.com>
2021-12-06 21:25:44 -0300

    Projeto Git/Github Spread

---
+++ src/Spread/ParOuImpar.java
@@ -0,0 +1,26 @@
+package Spread;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		int numero = sc.nextInt();
+		
+		if(numero % 2 !=0) {
+			System.out.println("ÍMPAR");
+		}else {
+			System.out.println("PAR");
+		}
+		
+		
+		
+		sc.close();
+
+	}
+
+}
